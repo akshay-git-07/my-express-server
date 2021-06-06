7c877ce3d4abd9363d091216ece98198dc88cdc4
akshay-git-07 <akshaybag7@gmail.com>
2021-06-06 21:12:17 +0530

    Test Version 1.0

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.get("/", function(request, response){
+    response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/contact", function(req, res) {
+    res.send("Contact me at: akshay@gmail.com");
+});
+
+app.get("/about", function(req, res) {
+    res.send("I am a Akshay & I am a Web Developer");
+});
+
+app.get("/hobbies", function(req, res) {
+    res.send("<ul><li>Eat</li><li>Code</li><li>Sleep</li><li>Repeat</li></ul>");
+});
+
+app.listen(3000, function (){
+    console.log("Server is listening on port 3000");
+});
+
+// If nodemon fails to run then run the following command:
+// Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
